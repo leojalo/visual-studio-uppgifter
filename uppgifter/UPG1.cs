842c6201ca7e7e49f0a17a6222799ded5aa2d089
leojalo <ljalo2008@gmail.com>
2024-11-20 08:51:30 +0200

    Add files via upload

---
+++ uppgifter/UPG1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPPGIFT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Leo Jalo";
+            System.Console.WriteLine(name);
+            int number1 = 1;
+            System.Console.WriteLine(number1);
+            int number2 = 2;
+            System.Console.WriteLine(number2);
+        }
+    }
+}
