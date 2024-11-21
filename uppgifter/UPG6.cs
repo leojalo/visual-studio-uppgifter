165fcaa6b4b8d4f051a8e305de6809d7ddccd0b6
leojalo <ljalo2008@gmail.com>
2024-11-21 09:42:24 +0200

    Add files via upload

---
+++ uppgifter/UPG6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            
+
+            //Loop thing
+            Console.Write("How many times do you want to repeat the message? :  ");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+            if (loopCounter <= 0)
+            {
+                Console.WriteLine("Sorry, Please enter a value above 0");
+            }
+            else
+            {
+                for (int i = 0; i < loopCounter; i++)
+                {
+                    Console.WriteLine("loop " + i);
+                }
+                Console.WriteLine("The loop was succesful");
+            }
+            Console.ReadLine();
+        }
+    }
+}
