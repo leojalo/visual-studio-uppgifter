be592caa5ee99a1c30b83223cc5b81f721b74b34
leojalo <ljalo2008@gmail.com>
2024-11-21 08:53:24 +0200

    Add files via upload

---
+++ uppgifter/UPG3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPG3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Go home " + name + "!");
+        }
+    }
+}
