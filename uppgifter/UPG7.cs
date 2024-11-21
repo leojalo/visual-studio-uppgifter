0b538dba2a9bc0a627a66c7929d3764bb6ede578
leojalo <ljalo2008@gmail.com>
2024-11-21 11:44:53 +0200

    Add files via upload

---
+++ uppgifter/UPG7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPG7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Counter = 1;
+
+            while (Counter <= 5)
+            {
+                Console.WriteLine("Counter: " + Counter);
+                Counter++;
+            }
+
+            Console.WriteLine("The loop has finished.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
