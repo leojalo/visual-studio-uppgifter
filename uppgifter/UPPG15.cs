2ef9d579c7aa8dea01b9b6c00ce0ccc55531c757
leojalo <ljalo2008@gmail.com>
2024-12-04 09:43:23 +0200

    Add files via upload

---
+++ uppgifter/UPPG15.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPPG15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 5, 25, 30 };
+            int max = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+
+            Console.WriteLine($"The maximum value in the array is: {max}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
