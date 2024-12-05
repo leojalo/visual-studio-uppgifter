f2a00e3404ec6d25511ef26bd4328ecdc0771e11
leojalo <ljalo2008@gmail.com>
2024-12-05 09:16:42 +0200

    Add files via upload

---
+++ uppgifter/UPPG20.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UPPG20
+{
+    class Circle
+    {
+        
+        public double Radius { get; private set; }
+
+        
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        // Calculates the area of the circle
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Circle circle = new Circle(1.5);
+
+            // Calculates the area of the circle
+            double area = circle.CalculateArea();
+
+            // Prints the area of the circle in the console
+            Console.WriteLine($"Area of the circle is: {area}");
+        }
+    }
+}
