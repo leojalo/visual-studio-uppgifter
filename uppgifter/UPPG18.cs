2a0c5ef0203e5bb37c26ffb26cbb9cc74d296fe3
leojalo <ljalo2008@gmail.com>
2024-12-05 09:04:58 +0200

    Add files via upload

---
+++ uppgifter/UPPG18.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UPPG18
+{
+    class Program
+    {
+        // Function to calculate the area of your triangle
+        static double CalculateArea(double width, double height)
+        {
+            return (width * height) / 2;
+        }
+
+        static void Main(string[] args)
+        {
+            // Ask the user for the width of the triangle
+            Console.Write("Please enter the width of the triangle: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            // Ask the user for the height of the triangle
+            Console.Write("Please enter the height of the triangle: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            // Calculates the area of your triangle
+            double area = CalculateArea(width, height);
+
+            // Prints your triangles area
+            Console.WriteLine($"The area of your triangle is {area}");
+        }
+    }
+}
