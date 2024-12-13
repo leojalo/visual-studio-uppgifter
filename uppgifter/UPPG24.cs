e433d68ab0f64798628d1622d5d5635094e7435f
leojalo <ljalo2008@gmail.com>
2024-12-13 08:56:05 +0200

    Add files via upload

---
+++ uppgifter/UPPG24.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPPG24
+{
+
+    class product
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+d
